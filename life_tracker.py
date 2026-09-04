a3a10522e8174d2cb6fce385acd5af3649589af1
Ryan Thalis <ryan.thalis9@gmail.com>
2026-09-04 12:16:58 -0400

    Add CLI input validation

--- life_tracker.py
+++ life_tracker.py
@@ -1,4 +1,5 @@
 import db
+import datetime as dt
 
 
 
@@ -13,22 +14,27 @@ def menu2():
     print("[1] View Workouts")  
     print("[2] View Study sessions")         
 
-def get_int(prompt: str, min_value = 1) -> int: 
+def get_int(prompt: str, min_value: int = 1, max_value: int | None = None,) -> int:
+    while True:
 
-        while True: 
+        try:
+            x = int(input(prompt).strip())
+            print()
         
-            try:
-                x = int(input(prompt).strip())
-                print()
-            
-                if x < min_value:
-                    print(f"number has to be greater than {min_value}")
-                    continue
-                
-            except ValueError:
-                print("incorrect value try again")
+            if x < min_value:
+                print(f"number has to be greater than or equal to {min_value}")
                 continue
-            return x 
+
+            if max_value is not None:
+                if x > max_value:
+                    print(f"number has to be smaller than or equal to {max_value}")
+                    continue
+
+        except ValueError:
+            print("incorrect value try again")
+            continue
+
+        return x
     
 def get_summary(workouts, studies):
 
@@ -100,6 +106,34 @@ def print_studies_grouped(studies):
             print(f"{line}. {subjects} {mins} minutes")
         print(f"Total: {total_mins} minutes")
 
+def get_nonempty(prompt: str) -> str:
+
+    while True:
+        value = input(prompt)
+
+        if value.strip() == "":
+            print("Input must not be empty ")
+            continue
+
+        return value.strip()
+
+def get_date(prompt: str) -> str:
+
+    while True:
+        date = input(prompt).strip()
+
+        try:
+            date_val = dt.date.fromisoformat(date)
+            parse_date = date_val.isoformat()
+            if parse_date == date:
+                return parse_date
+            else:
+                print("Invalid format, enter date again")
+                continue
+
+        except ValueError:
+            print("Invalid format, enter date again")
+            continue
 
 def main():
     db.init_db()
@@ -107,13 +141,13 @@ def main():
     while True:
         print()
         menu()
-        option = get_int(("please choose an option from between 1-5: "))
+        option = get_int("please choose an option from between 1-5: ", 1, 5)
         print()       
 
         if option == 1:
             print("You have chosen to log a workout")
-            date_value = input("Please enter the date of your workout (yyyy-mm-dd): ").strip()
-            exercise_value = input("Please enter the exercise: ").strip()
+            date_value = get_date("Please enter the date of your workout (yyyy-mm-dd): ")
+            exercise_value = get_nonempty("Please enter the exercise: ")
             sets_value = get_int("Please enter the number of sets: ")
             notes = input("please enter any relevant notes: ")
 
@@ -123,8 +157,8 @@ def main():
         
         elif option == 2:
             print("You have chosen to log a study session")
-            study_date = input("Please enter the date of your study session (yyyy-mm-dd): ").strip()
-            study_subject = input("Please enter the subject of study: ").strip()
+            study_date = get_date("Please enter the date of your study session (yyyy-mm-dd): ")
+            study_subject = get_nonempty("Please enter the subject of study: ")
             study_minutes = get_int("Please enter how long you studied for in minutes")
             notes = input("please enter any relevant notes: ")
 
@@ -142,13 +176,9 @@ def main():
         
             while True:
 
-                option_workout_study = get_int("please choose an option from between 1-2: ")    
-
-                if option_workout_study > 2 or option_workout_study < 1:
-                    print("This is out of the range of options try again: ")
-                    continue
+                option_workout_study = get_int("please choose an option from between 1-2: ", 1, 2)
             
-                elif option_workout_study == 1:
+                if option_workout_study == 1:
                     workouts = db.get_entries("workout")
 
                     print_workouts_grouped(workouts)                
@@ -160,9 +190,6 @@ def main():
                     print_studies_grouped(studies)
                     break
                 
-        if option < 1 or option > 5:
-            print("This is out of the range of options try again: ")
-            continue
 
         elif option == 4:
             workouts = db.get_entries("workout")
@@ -173,12 +200,9 @@ def main():
             print(f"total number of workouts: {a}\ntotal number of study sessions: {b}\ntotal number of sets: {c}\ntotal study minutes: {d}\ntopics studied: {e}")
                 
 
-        elif option == 5: 
+        elif option == 5:
             print("You will now exit the program")
             break
 
 if __name__ == "__main__":
     main()
-
-
-    
